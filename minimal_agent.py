ac0f2d965cdbd149eb2466b306c5f14659faf874
Joshua Carroll <joshua.carroll@snowflake.com>
2023-06-26 17:56:55 -0700

    Update minimal_agent to ddg-search

--- minimal_agent.py
+++ minimal_agent.py
@@ -1,10 +1,10 @@
-from langchain import OpenAI
+from langchain.llms import OpenAI
 from langchain.agents import AgentType, initialize_agent, load_tools
 from langchain.callbacks import StreamlitCallbackHandler
 import streamlit as st
 
 llm = OpenAI(temperature=0, streaming=True)
-tools = load_tools(["serpapi"], llm=llm)
+tools = load_tools(["ddg-search"])
 agent = initialize_agent(
     tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
 )
