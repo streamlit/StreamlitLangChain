34fbfa060f8b68090a2a27993a93f6563a3613b4
Joshua Carroll <joshua.carroll@snowflake.com>
2023-06-30 14:35:44 -0700

    Fix placeholder

--- streaming_demo.py
+++ streaming_demo.py
@@ -25,7 +25,7 @@ if "messages" not in st.session_state:
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-if prompt := st.chat_input(placeholder="Who won the Women's U.S. Open in 2018?"):
+if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
 
